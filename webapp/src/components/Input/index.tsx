adbe6f7174a310953d22809ad281b4a8d782050d
Sergei Dmitriev <iserdmi@gmail.com>
2023-09-10 18:17:49 +0200

    Add formik

--- webapp/src/components/Input/index.tsx
+++ webapp/src/components/Input/index.tsx
@@ -1,14 +1,8 @@
-export const Input = ({
-  name,
-  label,
-  state,
-  setState,
-}: {
-  name: string
-  label: string
-  state: Record<string, any>
-  setState: React.Dispatch<React.SetStateAction<any>>
-}) => {
+import { type FormikProps } from 'formik'
+
+export const Input = ({ name, label, formik }: { name: string; label: string; formik: FormikProps<any> }) => {
+  const value = formik.values[name]
+
   return (
     <div style={{ marginBottom: 10 }}>
       <label htmlFor={name}>{label}</label>
@@ -16,9 +10,9 @@ export const Input = ({
       <input
         type="text"
         onChange={(e) => {
-          setState({ ...state, [name]: e.target.value })
+          void formik.setFieldValue(name, e.target.value)
         }}
-        value={state[name]}
+        value={value}
         name={name}
         id={name}
       />
