f1f453712c014e2240a55c39ac85b4d22908fd08
Sergei Dmitriev <iserdmi@gmail.com>
2023-09-17 16:35:28 +0200

    Add pumpGetRoute function

--- webapp/src/pages/ideas/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ideas/ViewIdeaPage/index.tsx
@@ -1,7 +1,6 @@
 import type { TrpcRouterOutput } from '@ideanick/backend/src/router'
 import { canBlockIdeas, canEditIdea } from '@ideanick/backend/src/utils/can'
 import format from 'date-fns/format'
-import { useParams } from 'react-router-dom'
 import { Alert } from '../../../components/Alert'
 import { Button, LinkButton } from '../../../components/Button'
 import { FormItems } from '../../../components/FormItems'
@@ -9,7 +8,7 @@ import { Icon } from '../../../components/Icon'
 import { Segment } from '../../../components/Segment'
 import { useForm } from '../../../lib/form'
 import { withPageWrapper } from '../../../lib/pageWrapper'
-import { getEditIdeaRoute, type ViewIdeaRouteParams } from '../../../lib/routes'
+import { getEditIdeaRoute, getViewIdeaRoute } from '../../../lib/routes'
 import { trpc } from '../../../lib/trpc'
 import css from './index.module.scss'
 
@@ -69,7 +68,7 @@ const BlockIdea = ({ idea }: { idea: NonNullable<TrpcRouterOutput['getIdea']['id
 
 export const ViewIdeaPage = withPageWrapper({
   useQuery: () => {
-    const { ideaNick } = useParams() as ViewIdeaRouteParams
+    const { ideaNick } = getViewIdeaRoute.useParams()
     return trpc.getIdea.useQuery({
       ideaNick,
     })
