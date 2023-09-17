973f7d1f599eef4e6e0c50885d32fc75336a71d1
Sergei Dmitriev <iserdmi@gmail.com>
2023-09-17 15:21:55 +0200

    Add brevo

--- backend/src/lib/emails.ts
+++ backend/src/lib/emails.ts
@@ -4,6 +4,7 @@ import { type Idea, type User } from '@prisma/client'
 import fg from 'fast-glob'
 import Handlebars from 'handlebars'
 import _ from 'lodash'
+import { sendEmailThroughBrevo } from './brevo'
 import { env } from './env'
 
 const getHbrTemplates = _.memoize(async () => {
@@ -42,11 +43,12 @@ const sendEmail = async ({
       homeUrl: env.WEBAPP_URL,
     }
     const html = await getEmailHtml(templateName, fullTemplateVaraibles)
+    const { loggableResponse } = await sendEmailThroughBrevo({ to, html, subject })
     console.info('sendEmail', {
       to,
-      subject,
       templateName,
-      html,
+      templateVariables,
+      response: loggableResponse,
     })
     return { ok: true }
   } catch (error) {
