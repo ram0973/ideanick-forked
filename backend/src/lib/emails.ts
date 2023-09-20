4f56ff54dd65f281c10c598028415e8120411157
Sergei Dmitriev <iserdmi@gmail.com>
2023-09-20 16:46:28 +0200

    Standartify logger input, serialize error

--- backend/src/lib/emails.ts
+++ backend/src/lib/emails.ts
@@ -46,7 +46,7 @@ const sendEmail = async ({
     }
     const html = await getEmailHtml(templateName, fullTemplateVaraibles)
     const { loggableResponse } = await sendEmailThroughBrevo({ to, html, subject })
-    logger.info('sendEmail', {
+    logger.info('email', 'sendEmail', {
       to,
       templateName,
       templateVariables,
@@ -54,7 +54,11 @@ const sendEmail = async ({
     })
     return { ok: true }
   } catch (error) {
-    logger.error(error)
+    logger.error('email', error, {
+      to,
+      templateName,
+      templateVariables,
+    })
     return { ok: false }
   }
 }
